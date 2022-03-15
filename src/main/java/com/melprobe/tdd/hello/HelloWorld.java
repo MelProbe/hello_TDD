8d112e8fae5e8444607ddda8ed9783d788602d60
MelProbe <melprobe@yonsei.ac.kr>
2022-03-15 11:47:07 +0900

    🎉 init #1

---
+++ src/main/java/com/melprobe/tdd/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.melprobe.tdd.hello;
+
+public class HelloWorld {
+
+    public String getMessage() {
+        return "Hello World";
+    }
+
+    public String getMessage(String name) {
+        if (name == null) {
+            name = "World";
+        }
+        
+        return "Hello " + name;
+    }
+
+}
---
+++ src/test/java/com/melprobe/tdd/hello/HelloWorldTest.java
@@ -0,0 +1,43 @@
+package com.melprobe.tdd.hello;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+    private HelloWorld hello;
+    
+    @Before
+    public void setup() {
+        hello = new HelloWorld();
+    }
+
+    @Test
+    public void test() {
+        String msg = hello.getMessage();
+        
+        assertEquals("Hello World", msg);
+    }
+    
+    @Test
+    public void testGetMessage() {
+        String msg = hello.getMessage("MelProbe");
+        
+        assertEquals("Hello MelProbe", msg);
+    }
+    
+    @Test
+    public void testGetMessageNull() {
+        String msg = hello.getMessage(null);
+        
+        assertEquals("Hello World", msg);
+    }
+    
+    @After
+    public void tearDown() {
+        System.out.println("...");
+    }
+
+}
